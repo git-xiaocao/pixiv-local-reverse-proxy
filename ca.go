9713dd24dcc4a9c4060ae139aa7a018c52d9011a
xiao-cao-x <xiao-cao-x@hotmail.com>
2022-03-28 16:36:11 +0800

    更新ca

--- ca.go
+++ ca.go
@@ -2,8 +2,8 @@ package PixivLocalReverseProxy
 
 import _ "embed"
 
-//go:embed ca/ca.crt
+//go:embed ca/ca.pem
 var caCert []byte
 
-//go:embed ca/ca.key
+//go:embed ca/ca.key.pem
 var caKey []byte
